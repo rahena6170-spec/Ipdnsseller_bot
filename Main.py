29cc59dc4a6c83b6fcc5abd9ccc210743d126dc7
rahena6170-spec <rahena6170@gmail.com>
2026-09-13 01:17:45 +0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -33,7 +33,7 @@ def send_welcome(message):
     btn2 = types.InlineKeyboardButton("📱 IP for WhatsApp & Instagram", callback_data='cat_social')
     btn3 = types.InlineKeyboardButton("🌐 Premium DNS", callback_data='cat_dns')
     # 🆕 New Category: Gmail Tools & Setup Video
-    btn_tools = types.InlineKeyboardButton("📹 Gmail Tools & Setup Video ($3)", callback_data='pkg_Gmail Tools & Setup Video ($3)')
+    btn_tools = types.InlineKeyboardButton("📹 Gmail Tools & Setup Video ($5)", callback_data='pkg_Gmail Tools & Setup Video ($3)')
     btn4 = types.InlineKeyboardButton("💬 Support / Admin", url=f"https://t.me/{ADMIN_HANDLE}")
     
     markup.add(btn1, btn2, btn3, btn_tools, btn4)
